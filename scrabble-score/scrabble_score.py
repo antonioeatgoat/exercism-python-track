f1d01491b05c436f75040d0fb05a6fdb2d7c130c
antoniomangiacapra <antonio.mangiacapra@drivek.com>
2020-03-30 11:53:30 +0200

    scrabble-score: complete exercise

---
+++ scrabble-score/scrabble_score.py
@@ -0,0 +1,37 @@
+def score(word):
+    return sum([_get_points_by_letter(x) for x in word])
+
+
+def _get_points_by_letter(letter: str) -> int:
+    return _points_table()[letter.lower()]
+
+
+def _points_table() -> []:
+    return {
+        'a': 1,
+        'b': 3,
+        'c': 3,
+        'd': 2,
+        'e': 1,
+        'f': 4,
+        'g': 2,
+        'h': 4,
+        'i': 1,
+        'j': 8,
+        'k': 5,
+        'l': 1,
+        'm': 3,
+        'n': 1,
+        'o': 1,
+        'p': 3,
+        'q': 10,
+        'r': 1,
+        's': 1,
+        't': 1,
+        'u': 1,
+        'v': 4,
+        'w': 4,
+        'x': 8,
+        'y': 4,
+        'z': 10
+    }
