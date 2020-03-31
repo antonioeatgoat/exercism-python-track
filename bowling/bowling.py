ca3d1bc06f8761cefdd8168eef4150dc76ff7945
antoniomangiacapra <antonio.mangiacapra@drivek.com>
2020-03-31 19:08:04 +0200

    bowling: simplify the logic of the bowling game class
    Just adding a bit of complexity to the game-finished check, it's possible to make the rest of the class much small and simpler

--- bowling/bowling.py
+++ bowling/bowling.py
@@ -11,9 +11,6 @@ class BowlingGame:
     _frames_score = {}
 
     _remaining_frames = 10
-    _remaining_rolls = 2
-    _remaining_bonus = 0
-
     _bonus_throws = 0
 
     def __init__(self):
@@ -32,13 +29,7 @@ class BowlingGame:
 
         self._handle_single_roll(pins)
 
-        if self._is_strike():
-            self._handle_strike()
-
-        if self._is_spare():
-            self._handle_spare()
-
-        if self._is_frame_finished() and not self._is_game_finished():
+        if self._is_frame_finished():
             self._update_frame_count()
             self._setup_next_frame()
 
@@ -49,43 +40,29 @@ class BowlingGame:
 
     def _handle_single_roll(self, pins):
         """Updates counters related to a generic single ball throw"""
-        self._remaining_rolls -= 1
         self._remaining_spins -= pins
         self._frames_score[self._current_frame()].append(pins)
-        if self._remaining_frames == 0 and self._remaining_bonus > 0:
-            self._remaining_bonus -= 1
-
-    def _handle_strike(self):
-        """Updates counters specific of a strike throw"""
-        if self._is_second_last_frame():
-            self._remaining_bonus = 1
-        if self._is_last_frame():
-            self._remaining_bonus = 2
-
-    def _handle_spare(self):
-        """Updates counters specific of a spare throw"""
-        if self._is_last_frame():
-            self._remaining_bonus = 1
 
     def _update_frame_count(self):
         self._remaining_frames -= 1
 
     def _setup_next_frame(self):
         self._frames_score[self._current_frame()] = []
-        self._remaining_rolls = 2
         self._remaining_spins = 10
 
     def _is_strike(self):
-        return self._remaining_rolls == 1 and self._remaining_spins == 0
-
-    def _is_spare(self):
-        return self._remaining_rolls == 0 and self._remaining_spins == 0
+        return self._get_remaining_rolls() == 1 and self._remaining_spins == 0
 
     def _is_frame_finished(self):
-        return self._remaining_rolls == 0 or self._is_strike()
+        return self._get_remaining_rolls() == 0 or self._is_strike()
 
     def _is_game_finished(self):
-        return self._remaining_frames == 0 and self._remaining_bonus == 0
+        return self._remaining_frames == 0 \
+               and (sum(self._frames_score[9]) != 10 or len(self._frames_score[9]) != 1 and self._get_remaining_rolls() == 1) \
+               or self._remaining_frames == -1 and len(self._frames_score[10]) != 1
+
+    def _get_remaining_rolls(self) -> int:
+        return 2 - len(self._frames_score[self._current_frame()])
 
     def _current_frame(self):
         return 10 - self._remaining_frames
